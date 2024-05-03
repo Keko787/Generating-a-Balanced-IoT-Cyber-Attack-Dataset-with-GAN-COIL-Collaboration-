d255b0a9ddc7f6adaea7772d02d7379207a812a0
Dave West <dawest6674@eagle.fgcu.edu>
2024-05-03 13:47:24 -0400

    First implimentation of naive random oversampling by Tim Meinart.

--- classical_sampling_techniques/oversampling/naive_random.py
+++ classical_sampling_techniques/oversampling/naive_random.py
@@ -1,71 +1,25 @@
 #########################################################
 #    Imports    #
 #########################################################
-import tensorflow as tf
+
 
 # List all physical devices and configure them before any other operations
-gpus = tf.config.list_physical_devices('GPU')
-if gpus:
-    try:
-        # Set memory growth on the GPU to true
-        for gpu in gpus:
-            tf.config.experimental.set_memory_growth(gpu, True)
-            print("Memory growth set")
-            print("GPU Device:", gpu, "\n")
-    except RuntimeError as e:
-        # Memory growth must be set before initializing the GPUs
-        print("RuntimeError in setting up GPU:", e)
-
-    try:
-        # Optional: Set a memory limit
-        memory_limit = 8000  # e.g., 4096 MB for 4GB
-        config = tf.config.experimental.VirtualDeviceConfiguration(memory_limit=memory_limit)
-        tf.config.experimental.set_virtual_device_configuration(gpus[0], [config])
-        print(f"Memory limit set to {memory_limit}MB on GPU {gpus[0].name}")
-    except RuntimeError as e:
-        print(f"Failed to set memory limit: {e}")
-else:
-    print("No GPU devices found.")
-
 import numpy as np
 import pandas as pd
-import math
-import glob
+from imblearn.over_sampling import RandomOverSampler  # Import the RandomOverSampler
 import random
 from tqdm import tqdm
 # from IPython.display import clear_output
 import os
-import time
-import matplotlib.pyplot as plt
-import seaborn as sns
-
-from ydata_synthetic.synthesizers.regular import RegularSynthesizer
-from ydata_synthetic.synthesizers import ModelParameters, TrainParameters
-
-import sklearn.cluster as cluster
-from sklearn.model_selection import cross_val_score
-from sklearn.model_selection import cross_val_predict
-from sklearn.model_selection import KFold
-from sklearn.preprocessing import StandardScaler, LabelEncoder
+from sklearn.preprocessing import RobustScaler, LabelEncoder
 from sklearn.utils import shuffle
-from sklearn.impute import SimpleImputer
-from sklearn.pipeline import Pipeline
-from sklearn.metrics import confusion_matrix
-from sklearn.metrics import matthews_corrcoef
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score, f1_score
 
-# Print versions and device configurations after ensuring GPU settings
-print("TensorFlow version:", tf.__version__)
-# print("CUDA version:", tf.sysconfig.get_build_info()['cuda_version'])
-# print("cuDNN version:", tf.sysconfig.get_build_info()['cudnn_version'])
-# print(tf.config.list_physical_devices(), "\n", tf.config.list_logical_devices(), "\n")
-# print(tf.config.list_physical_devices('GPU'), "\n")
+
 
 #########################################################
 #    Loading the CSV    #
 #########################################################
-DATASET_DIRECTORY = './archive/'  # If your dataset is within your python project directory, change this to the relative path to your dataset
+DATASET_DIRECTORY = './dataset/'  # If your dataset is within your python project directory, change this to the relative path to your dataset
 csv_filepaths = [filename for filename in os.listdir(DATASET_DIRECTORY) if filename.endswith('.csv')]
 
 print(csv_filepaths)
@@ -87,16 +41,16 @@ num_cols = [
     'Rate', 'Srate', 'ack_count', 'syn_count',
     'fin_count', 'urg_count', 'rst_count', 'Tot sum',
     'Min', 'Max', 'AVG', 'Std', 'Tot size', 'IAT', 'Number',
-    'Magnitue', 'Radius', 'Covariance', 'Variance', 'Weight', ]
+    'Magnitue', 'Radius', 'Covariance', 'Variance', 'Weight' ]
 cat_cols = [
     'Protocol Type', 'Drate', 'fin_flag_number', 'syn_flag_number', 'rst_flag_number',
     'psh_flag_number', 'ack_flag_number', 'ece_flag_number',
     'cwr_flag_number', 'HTTP', 'HTTPS', 'DNS', 'Telnet',
     'SMTP', 'SSH', 'IRC', 'TCP', 'UDP', 'DHCP', 'ARP',
-    'ICMP', 'IPv', 'LLC', ]
+    'ICMP', 'IPv', 'LLC' ]
 
 # feature scaling
-scaler = StandardScaler()
+scaler = RobustScaler()
 
 for data_set in tqdm(data_sets):
     scaler.fit(pd.read_csv(DATASET_DIRECTORY + data_set)[num_cols])
@@ -134,44 +88,54 @@ for data_set in data_sets:
     df = pd.read_csv(data_path)
     full_data = pd.concat([full_data, df])
 
+# Apply random over-sampling
+ros = RandomOverSampler(random_state=42)
+X = full_data.drop('label', axis=1)
+y = full_data['label']
+X_res, y_res = ros.fit_resample(X, y)
+
+# Combine the resampled features and labels back into a single DataFrame
+full_data_resampled = pd.DataFrame(X_res, columns=X.columns)
+full_data_resampled['label'] = y_res
+
 # prints an instance of each class
 print("Before encoding:")
-unique_labels = full_data['label'].unique()
+unique_labels = full_data_resampled['label'].unique()
 for label in unique_labels:
     print(f"First instance of {label}:")
-    print(full_data[full_data['label'] == label].iloc[0])
+    print(full_data_resampled[full_data_resampled['label'] == label].iloc[0])
 
 # Shuffle data
-full_data = shuffle(full_data, random_state=1)
+full_data_resampled = shuffle(full_data_resampled, random_state=1)
 
 # Scale the features in the dataframe
-full_data[num_cols] = scaler.transform(full_data[num_cols])
+full_data_resampled[num_cols] = scaler.fit_transform(full_data_resampled[num_cols])
 
 # prove if the data is loaded properly
 print("Real data:")
-print(full_data[:2])
-print(full_data.shape)
+print(full_data_resampled[:2])
+print(full_data_resampled.shape)
 
 # Relabel the 'label' column using dict_7classes
-# full_data['label'] = full_data['label'].map(dict_7classes)
+# full_data_resampled['label'] = full_data_resampled['label'].map(dict_7classes)
 
 # # Relabel the 'label' column using dict_2classes
-# full_data['label'] = full_data['label'].map(dict_2classes)
+# full_data_resampled['label'] = full_data_resampled['label'].map(dict_2classes)
 
 # Assuming 'label' is the column name for the labels in the DataFrame `synth_data`
-unique_labels = full_data['label'].nunique()
+unique_labels = full_data_resampled['label'].nunique()
 
 # Print the number of unique labels
 print(f"There are {unique_labels} unique labels in the dataset.")
 
-class_counts = full_data['label'].value_counts()
+class_counts = full_data_resampled['label'].value_counts()
 print(class_counts)
 
 # Display the first few entries to verify the changes
-print(full_data)
+print(full_data_resampled)
 
 # prep the data to be inputted into model
-data = full_data
+data = full_data_resampled
 
 #########################################################
 #    Preprocessing / Clustering of Class Data    #
@@ -179,7 +143,7 @@ data = full_data
 
 # encodes the label
 label_encoder = LabelEncoder()
-full_data['label'] = label_encoder.fit_transform(full_data['label'])
+full_data_resampled['label'] = label_encoder.fit_transform(full_data_resampled['label'])
 
 # Store label mappings
 label_mapping = {index: label for index, label in enumerate(label_encoder.classes_)}
@@ -193,7 +157,7 @@ print("After encoding:")
 for label, code in class_codes.items():
     # Print the first instance of each class
     print(f"First instance of {label} (code {code}):")
-    print(full_data[full_data['label'] == code].iloc[0])
+    print(full_data_resampled[full_data_resampled['label'] == code].iloc[0])
 
 # # Ensure all data for clustering is numeric
 # clustering_features = [col for col in num_cols if col in minority_class_data.columns]  # Ensure these are only numeric
@@ -209,20 +173,20 @@ for label, code in class_codes.items():
 # print(cluster_counts)
 #
 # # Merging this back to the full dataset if needed
-# full_data.loc[full_data['label'] == 'Benign', 'Cluster'] = labels
+# full_data_resampled.loc[full_data_resampled['label'] == 'Benign', 'Cluster'] = labels
 #
 # # If 'Cluster' column has been created and needs encoding
-# if 'Cluster' in full_data.columns:
-#     full_data['Cluster'] = label_encoder.fit_transform(full_data['Cluster'])
+# if 'Cluster' in full_data_resampled.columns:
+#     full_data_resampled['Cluster'] = label_encoder.fit_transform(full_data_resampled['Cluster'])
 #
 # # Impute NaN values in 'label' and 'Cluster' with the mode (most frequent value)
 # for column in ['label', 'Cluster']:
-#     mode_value = full_data[column].mode()[0]
-#     full_data[column].fillna(mode_value, inplace=True)
+#     mode_value = full_data_resampled[column].mode()[0]
+#     full_data_resampled[column].fillna(mode_value, inplace=True)
 #
-# print(full_data[['label', 'Cluster']].isna().sum())
+# print(full_data_resampled[['label', 'Cluster']].isna().sum())
 #
 # # Display some entries to verify the changes
-# print(full_data[['label', 'Cluster']].head())
+# print(full_data_resampled[['label', 'Cluster']].head())
 
-print(full_data.head())
+print(full_data_resampled.head())
